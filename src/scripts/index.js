218f0e1e26c976a21a998b6027df928153bde56d
arif_faturahman <arif.afrydo@gmail.com>
2021-06-09 22:05:19 +0700

    add App & delete console log

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,19 @@
 import 'regenerator-runtime';
 import '../styles/style.css';
 import '../styles/responsive.css';
+import App from './views/app';
 
-console.log('Hello Coders!');
+const app = new App({
+  button: document.querySelector('#hamburgerButton'),
+  drawer: document.querySelector('#navigationDrawer'),
+  content: document.querySelector('#mainContent'),
+
+});
+
+window.addEventListener('hashchange', () => {
+  app.renderpage();
+});
+
+window.addEventListener('load', () => {
+  app.renderpage();
+});
