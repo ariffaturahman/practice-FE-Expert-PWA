82ff6a0a86cac5d49da8f90ef6d3bafa8c757a73
arif_faturahman <arif.afrydo@gmail.com>
2021-06-09 22:04:41 +0700

    add API config

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: '14e3d983e1a3819b0501bb936cbd57f7',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
